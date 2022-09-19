793a8309783bda5100b221098227156bb28e2663
JJJaTTo <j.j.jatto@gmail.com>
2022-09-19 16:40:24 -0700

    swapp

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -5,15 +5,18 @@
  * using two input parameters
  *
  * @a: input parameter 1
- * 
+ *
  * @b: input parameter 2
  *
  * return: nothing.
 */
-void swap_int(int *a, int B
+void swap_int(int *a, int *b)
 {
 	int temp;
 
 	temp = *a;
+
+	*a = *b;
+
 	*b = temp;
 }
