1ebaaca4ec53f9f35393a05f93f16441c87bff25
JJJaTTo <j.j.jatto@gmail.com>
2022-09-19 16:35:35 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int - swaps the value of two integerd
+ * using two input parameters
+ *
+ * @a: input parameter 1
+ * 
+ * @b: input parameter 2
+ *
+ * return: nothing.
+*/
+void swap_int(int *a, int B
+{
+	int temp;
+
+	temp = *a;
+	*b = temp;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,4 +7,5 @@
 
 void reset_to_98(int *n);
 int _putchar(char c);
+void reset_to_98(int *n);
 #endif
