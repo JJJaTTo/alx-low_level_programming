ee55688ea70ad85d16215eff9004e952e849b87e
JJJaTTo <j.j.jatto@gmail.com>
2022-09-21 01:54:14 -0700

    pointer arrays and strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - a function that concatenates two strings.
+ *
+ * @str:
+ *
+ * return: 0
+ */
+char *_strcat(char *dest, char *src)
+{	
+	*_strcat(*dest,*src);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+char *_strcat(char *dest, char *src);
+_putchar(char c);
+
+#ENDIF
