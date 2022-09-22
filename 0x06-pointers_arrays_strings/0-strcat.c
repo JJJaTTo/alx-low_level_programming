0d772a482b1254f8f6236b0a7b5db9326deffa07
JJJaTTo <j.j.jatto@gmail.com>
2022-09-22 02:27:51 -0700

    Jt

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,15 +8,7 @@
  *
  * Return: A pointer to the destination string @dest.
  */
-char *_strcat(char *dest, char *src)
+int main(void)
 {
-	int index = 0, dest_len = 0;
-
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
-	return (dest);
+	char *_strcat(char *dest, char *src)
 }
